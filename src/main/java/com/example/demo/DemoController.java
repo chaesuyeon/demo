564b34bb2610ed9f9998b46318f8957b21a9adb1
채수연 <suyeon030502@gmail.com>
2025-09-17 14:06:38 +0900

    UPDATE

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,5 +1,4 @@
 package com.example.demo;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,13 +7,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class DemoController {
     @GetMapping("/hello") // 전송 방식 GET
     public String hello(Model model) {
-        model.addAttribute("data", "반갑습니다."); // model 설정
+        model.addAttribute("data", "반갑습니다."); 
         return "hello"; // hello.html 연결
     }
 
-    @GetMapping("/")   // 루트 경로 처리
-public String index() {
-    return "index";  // Index.html을 렌더링 (대소문자 주의!)
+    @GetMapping("/about_detailed")   
+public String about() {
+    return "about_detailed";  
 }
+
+
 
 }
